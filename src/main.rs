65cf612cd4030dbdf5870950dc47a3c5bb682c13
MicaelMoon <sjodin.micael@gmail.com>
2025-11-28 04:09:35 +0100

    Model now returns json object that contains the message along with other data. It crashed when trying to read the messagee but not whilee saving in database

--- src/main.rs
+++ src/main.rs
@@ -16,33 +16,48 @@ use crate::db::campaign::insert_campaign;
 use crate::db::ollama_message;
 
 #[derive(Deserialize)]
-struct Player {
-    name: String,
+struct PlayerStats {
+    max_health: i32,
+    current_heaelth: i32,
+    max_attack: i32,
+    current_attack: i32,
+    max_defense: i32,
+    current_defense: i32,
+}
+
+#[derive(Deserialize)]
+struct Response {
+    player_stats: PlayerStats,
+    entities: serde_json::Value,
+    message: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // send_message("What did I ask you now again?").await;
-    //get_player();
 
-    //create_ollama_message_debug();
+    let prompt = "I follow the path in the woods";
 
-    send_prompt(
-        "I look around in search for a path where I can sneak into the center building. Alternativly I look for cellar leading underground of somee kind.",
-    ).await;
+    create_ollama_message_debug();
+
+    send_prompt(prompt).await;
 
     Ok(())
 }
 
 async fn send_prompt(prompt: &str) -> Result<String, String> {
-    let mut message: String = "".to_string();
+    let mut json_response: String = "".to_string();
 
     match generator_agent::send_message(prompt).await {
-        Ok(message) => println!("Ollama: {}", message),
+        Ok(response) => json_response = response,
         Err(e) => eprintln!("Error sending message: {}", e),
     };
 
-    Ok(message)
+    let parsed: Response = serde_json::from_str(&json_response).unwrap();
+
+    println!("Ollama: {}", parsed.message);
+
+    Ok(json_response)
 }
 
 fn create_ollama_message_debug() {
@@ -63,37 +78,6 @@ fn create_ollama_message_debug() {
     };
 
     ollama_message::insert_ollama_message(database, assistant_message);
-
-    let user_message = OllamaMessage {
-        id: None,
-        campaign_id: 1,
-        role: "user".to_string(),
-        content: "I follow the path untill I can see the fort in the distans. I then hide behind a bush to scout out the area".to_string(),
-        time_stamp: Utc::now().to_string(),
-    };
-
-    ollama_message::insert_ollama_message(database, user_message);
-
-    assistant_message = OllamaMessage {
-        id: None,
-        campaign_id: 1,
-        role: "assistant".to_string(),
-        content: "From your view you can see a massive fort the sice of a village. Headlights shooting out in every directions moving left and right.
-        Towers rise dozens of meeters up with sharpshooters. There is no obviouse path for you to sneak your way in from this angle.".to_string(),
-        time_stamp: Utc::now().to_string(),
-    };
-
-    ollama_message::insert_ollama_message(database, assistant_message);
-}
-
-fn get_player() -> Result<(), Box<dyn std::error::Error>> {
-    let data = fs::read_to_string("config/player.json")?;
-
-    let player: Player = serde_json::from_str(&data)?;
-
-    println!("Player name: {}", player.name);
-
-    Ok(())
 }
 
 async fn create_new_campaign() {
